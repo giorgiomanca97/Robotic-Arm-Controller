61d767214cb80dd1d0db97975f6c09119e88418d
giorgiomanca97 <giorgio.manca.97@gmail.com>
2023-08-07 10:42:27 +0200

    chore: reformat in arduino code

--- Robot/Controller.ino
+++ Robot/Controller.ino
@@ -18,53 +18,53 @@
 // ============================================================
 
 // DC Motors PINs
-#define MOTORS_EN   38      // Motors enabler
+#define MOTORS_EN     38    // Motors enabler
 
-#define MOTOR_1_INA 22      // Motor 1 spin direction
-#define MOTOR_1_INB 23      // Motor 1 spin direction
-#define MOTOR_1_PWM 5       // Motor 1 spin pwm
-#define MOTOR_1_CHA A8      // Motor 1 encoder A channel
-#define MOTOR_1_CHB A14     // Motor 1 encoder B channel
-#define MOTOR_1_END 37      // Motor 1 endstop switch
+#define MOTOR_1_INA   22    // Motor 1 spin direction
+#define MOTOR_1_INB   23    // Motor 1 spin direction
+#define MOTOR_1_PWM   5     // Motor 1 spin pwm
+#define MOTOR_1_CHA   A8    // Motor 1 encoder A channel
+#define MOTOR_1_CHB   A14   // Motor 1 encoder B channel
+#define MOTOR_1_END   37    // Motor 1 endstop switch
 
-#define MOTOR_2_INA 24      // Motor 2 spin direction
-#define MOTOR_2_INB 25      // Motor 2 spin direction
-#define MOTOR_2_PWM 2       // Motor 2 spin pwm
-#define MOTOR_2_CHA A9      // Motor 2 encoder A channel
-#define MOTOR_2_CHB A15     // Motor 2 encoder B channel
-#define MOTOR_2_END 36      // Motor 2 endstop switch
+#define MOTOR_2_INA   24    // Motor 2 spin direction
+#define MOTOR_2_INB   25    // Motor 2 spin direction
+#define MOTOR_2_PWM   2     // Motor 2 spin pwm
+#define MOTOR_2_CHA   A9    // Motor 2 encoder A channel
+#define MOTOR_2_CHB   A15   // Motor 2 encoder B channel
+#define MOTOR_2_END   36    // Motor 2 endstop switch
 
-#define MOTOR_3_INA 26      // Motor 3 spin direction
-#define MOTOR_3_INB 27      // Motor 3 spin direction
-#define MOTOR_3_PWM 3       // Motor 3 spin pwm
-#define MOTOR_3_CHA A10     // Motor 3 encoder A channel
-#define MOTOR_3_CHB 10      // Motor 3 encoder B channel
-#define MOTOR_3_END 35      // Motor 3 endstop switch
+#define MOTOR_3_INA   26    // Motor 3 spin direction
+#define MOTOR_3_INB   27    // Motor 3 spin direction
+#define MOTOR_3_PWM   3     // Motor 3 spin pwm
+#define MOTOR_3_CHA   A10   // Motor 3 encoder A channel
+#define MOTOR_3_CHB   10    // Motor 3 encoder B channel
+#define MOTOR_3_END   35    // Motor 3 endstop switch
 
-#define MOTOR_4_INA 28      // Motor 4 spin direction
-#define MOTOR_4_INB 29      // Motor 4 spin direction
-#define MOTOR_4_PWM 6       // Motor 4 spin pwm
-#define MOTOR_4_CHA A11     // Motor 4 encoder A channel
-#define MOTOR_4_CHB 11      // Motor 4 encoder B channel
-#define MOTOR_4_END 34      // Motor 4 endstop switch
+#define MOTOR_4_INA   28    // Motor 4 spin direction
+#define MOTOR_4_INB   29    // Motor 4 spin direction
+#define MOTOR_4_PWM   6     // Motor 4 spin pwm
+#define MOTOR_4_CHA   A11   // Motor 4 encoder A channel
+#define MOTOR_4_CHB   11    // Motor 4 encoder B channel
+#define MOTOR_4_END   34    // Motor 4 endstop switch
 
-#define MOTOR_5_INA 49      // Motor 5 spin direction
-#define MOTOR_5_INB 48      // Motor 5 spin direction
-#define MOTOR_5_PWM 7       // Motor 5 spin pwm
-#define MOTOR_5_CHA A12     // Motor 5 encoder A channel
-#define MOTOR_5_CHB 12      // Motor 5 encoder B channel
-#define MOTOR_5_END 33      // Motor 5 endstop switch
+#define MOTOR_5_INA   49    // Motor 5 spin direction
+#define MOTOR_5_INB   48    // Motor 5 spin direction
+#define MOTOR_5_PWM   7     // Motor 5 spin pwm
+#define MOTOR_5_CHA   A12   // Motor 5 encoder A channel
+#define MOTOR_5_CHB   12    // Motor 5 encoder B channel
+#define MOTOR_5_END   33    // Motor 5 endstop switch
 
-#define MOTOR_6_INA 47      // Motor 6 spin direction
-#define MOTOR_6_INB 46      // Motor 6 spin direction
-#define MOTOR_6_PWM 8       // Motor 6 spin pwm
-#define MOTOR_6_CHA A13     // Motor 6 encoder A channel
-#define MOTOR_6_CHB 13      // Motor 6 encoder B channel
-#define MOTOR_6_END 32      // Motor 6 endstop switch
+#define MOTOR_6_INA   47    // Motor 6 spin direction
+#define MOTOR_6_INB   46    // Motor 6 spin direction
+#define MOTOR_6_PWM   8     // Motor 6 spin pwm
+#define MOTOR_6_CHA   A13   // Motor 6 encoder A channel
+#define MOTOR_6_CHB   13    // Motor 6 encoder B channel
+#define MOTOR_6_END   32    // Motor 6 endstop switch
 
 // Other pins
-#define TOGGLE_COMM  52     // Toggle pin used to check timesampling
-#define TOGGLE_CTRL  53     // Toggle pin used to check timesampling
+#define TOGGLE_COMM   52    // Toggle pin used to check timesampling
+#define TOGGLE_CTRL   53    // Toggle pin used to check timesampling
 
 
 // ============================================================
